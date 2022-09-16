f296138c3dde8bc50b2595550287283b6f495d59
akkoucai <aimene.akkouche@grenoble-inp.org>
2022-09-17 01:23:16 +0200

    changed hero, feature and steps

--- src/config.mjs
+++ src/config.mjs
@@ -1,11 +1,11 @@
 export const SITE = {
-	name: 'AstroWind',
+	name: 'Cyberse',
 
-	origin: 'https://astrowind.vercel.app',
+	origin: 'http://localhost:3000',
 	basePathname: '/',
 
-	title: 'AstroWind — Your website with Astro + Tailwind CSS',
-	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
+	title: 'Cyberse',
+	description: '🚀 Cyberse is a free and ready to start template to make your website using Astro and Tailwind CSS.',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
 	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
