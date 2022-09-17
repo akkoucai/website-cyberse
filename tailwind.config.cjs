ec8deec1bca6b777b84b9b8a5e29cb16e979f9a0
akkoucai <aimene.akkouche@grenoble-inp.org>
2022-09-17 18:31:01 +0200

    add animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,10 +12,23 @@ module.exports = {
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
 			},
+			animation: {
+				fadeIn: "fadeIn 2s ease-in forwards"
+			},
+			keyframes: {
+				fadeIn: {
+				"0%": { opacity: 0 },
+				"100%": { opacity: 1 }
+				}
+			}
 		},
+		variants: {
+			animation: ["motion-safe"]
+		}
 	},
 	plugins: [require('@tailwindcss/typography')],
 	darkMode: 'class',
+	
 };
 
 /* 
